65048bc60a0ccec895abc4bfcfc5c3d6a857b948
VictorTBX <victoralberto6@hotmail.com>
2013-05-15 11:35:57 -0300

    Update DVD.java

--- DVD.java
+++ DVD.java
@@ -4,3 +4,10 @@ public class DVD {
   private int ano;
   private String estilo;
   private boolean disponivel;
+  
+  public DVD(int codigo, String titulo, int ano, String estilo) {
+    this.codigo = codigo;
+    this.titulo = titulo;
+    this.ano = ano;
+    this.estilo = estilo;
+  }
