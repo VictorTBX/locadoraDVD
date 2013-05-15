629ae78830b67192f440492d1233572563f0c0a5
VictorTBX <victoralberto6@hotmail.com>
2013-05-15 11:41:52 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,1 +1,30 @@
 public class Lista {
+  
+  private Node fim;
+  private Node inicio;
+  private int quantidade;
+  
+  public Lista() {
+    inicio = fim = null;
+    quantidade = 0;
+  }
+  
+  public boolean isEmpty() {
+    return quantidade == 0;
+  }
+  
+  public int size() {
+    return quantidade;
+  }
+  
+  public void addEnd(Node n) {
+    if(isEmpty) {
+      inicio = fim = n;
+    } else {
+      fim.setProximo(n);
+      n.setAnterior(fim);
+      n.setProximo(null);
+      fim = novo;      
+    } quantidade++;
+  }
+}
