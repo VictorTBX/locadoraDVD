86919e754542cda52b2a111861a64e2bee6040dd
VictorTBX <victoralberto6@hotmail.com>
2013-05-18 10:46:18 -0300

    Update DVD.java

--- DVD.java
+++ DVD.java
@@ -11,3 +11,35 @@ public class DVD {
     this.ano = ano;
     this.estilo = estilo;
   }
+  
+  public int getCodigo(){
+    return codigo;
+  }
+  
+  public void setCodigo(int codigo){
+    this.codigo = codigo;
+  }
+  
+  public String getTitulo(){
+    return titulo;
+  }
+  
+  public void setTitulo(String titulo){
+    this.titulo = titulo;
+  }
+  
+  public int getAno(){
+    return ano;
+  }
+  
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+  
+  public String getEstilo(){
+    return estilo;
+  }
+  
+  public void setEstilo(String estilo){
+    this.estilo = estilo;
+  }
