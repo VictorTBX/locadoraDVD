0a124587295328c8e4d73233571506a10e1a32c2
VictorTBX <victoralberto6@hotmail.com>
2013-05-18 11:09:31 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -9,4 +9,27 @@ public class Node {
     anterior = null;
   }
   
+  public DVD getDvd() {
+    return dvd;
+  }
   
+  public void setDvd(DVD dvd){
+    this.dvd = dvd;
+  }
+  
+  public Node getProximo() {
+    return proximo;
+  }
+  
+  public void setProximo(Node proximo){
+    this.proximo = proximo;
+  }
+  
+  public Node getAnterior() {
+    return anterior;
+  }
+  
+  public void setAnterior(Node anterior){
+    this.anterior = anterior;
+  }
+}
