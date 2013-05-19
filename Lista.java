4abbf8f1f4e237e86bb79b7733a8f60d12de82ce
VictorTBX <victoralberto6@hotmail.com>
2013-05-19 14:13:41 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,45 +1,64 @@
 public class Lista {
-  
-  private Node fim;
-  private Node inicio;
-  private int quantidade;
-  
-  public Lista() {
-    inicio = fim = null;
-    quantidade = 0;
-  }
-  
-  public boolean isEmpty() {
-    return quantidade == 0;
-  }
-  
-  public int size() {
-    return quantidade;
-  }
-  
-  public void addEnd(Node n) {
-    if(isEmpty) {
-      inicio = fim = n;
-    } else {
-      fim.setProximo(n);
-      n.setAnterior(fim);
-      n.setProximo(null);
-      fim = n;      
-    } quantidade++;
-  }
-  
-  public Node remove(DVD dvd){
-    if(isEmpty) {
-      return 
+    private Node inicio;
+    private Node fim;
+    private int quantidade;
+
+    public Lista() {
+        inicio = fim = null;
+        quantidade = 0;
     }
-  }
-  
-  public void display() {
+
+    public int size() {
+        return quantidade;
+    }
+
+    public boolean isEmpty() {
+        return quantidade == 0;
+    }
+
+    public void addEnd(Node n) {
+        if(isEmpty()) {
+            inicio = fim = n;
+        } else {
+            fim.setProximo(n);
+            n.setAnterior(fim);
+            n.setProximo(null);
+            fim = n;      
+        } quantidade++;
+    }
+
+    public Node getBegin() {
+        return inicio;
+    }
+
+    public Node getEnd() {
+        return fim;
+    } 
+
+    public Node remove(DVD dvd) {
+        String nome = dvd.getTitulo();
+        Node aux = inicio;
+        if (isEmpty()) {
+            throw new Error("Lista Vazia");
+        }
+        while(aux.getDvd().getTitulo().equals(nome) && (aux != null)) {
+            aux = aux.getProximo();
+        }
+        if (aux == null) {
+            throw new Error("Valor não encontrado");
+        } else {
+            aux.getAnterior().setProximo(aux.getProximo());
+            aux.getProximo().setAnterior(aux.getAnterior());
+            return aux;
+        }
+    }
+
+    public void display() {
         Node aux = inicio;
         for (int i = 0; i < quantidade; i++) {
             System.out.print(aux + " ");
             aux = aux.getProximo();
         }
         System.out.println("");
-    }  
+    }
 }
