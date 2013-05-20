115fb304065aca8b8a30efac4cb9ed189560ef08
VictorTBX <victoralberto6@hotmail.com>
2013-05-20 18:42:36 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -11,7 +11,7 @@ public class Node {
   
   @Override
     public String toString() {
-        return String.valueOf(dvd);
+        return String.valueOf(dvd.getTitulo());
     }
   
   public DVD getDvd() {
