52d51d549ec8159ba74d217a644605dd1ca7c9a3
VictorTBX <victoralberto6@hotmail.com>
2013-05-20 18:46:00 -0300

    Create DVD2.java

---
+++ DVD2.java
@@ -0,0 +1,55 @@
+public class DVD {
+  private int codigo;
+  private String titulo;
+  private int ano;
+  private String estilo;
+  private boolean disponivel;
+  
+  public DVD(int codigo, String titulo, int ano, String estilo) {
+    this.codigo = codigo;
+    this.titulo = titulo;
+    this.ano = ano;
+    this.estilo = estilo;
+    disponivel = true;
+  }
+  
+  public int getCodigo(){
+    return codigo;
+  }
+  
+  public void setCodigo(int codigo){
+    this.codigo = codigo;
+  }
+  
+  public String getTitulo(){
+    return titulo;
+  }
+  
+  public void setTitulo(String titulo){
+    this.titulo = titulo;
+  }
+  
+  public int getAno(){
+    return ano;
+  }
+  
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+  
+  public String getEstilo(){
+    return estilo;
+  }
+  
+  public void setEstilo(String estilo){
+    this.estilo = estilo;
+  }
+  
+  public boolean getDisponivel(){
+    return disponivel;
+  }
+  
+  public void setDisponivel(boolean disponivel){
+    this.disponivel = disponivel;
+  }
+}
