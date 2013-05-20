f75b0a075701ba09c5190f3f450987406e27c029
VictorTBX <victoralberto6@hotmail.com>
2013-05-20 18:44:45 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,20 @@ public class Main {
     public static void main(String[] args) {       
         
       Scanner in = new Scanner(System.in);
+      Scanner in2 = new Scanner(System.in);
         
         int resp, codigo;
         String nome;        
         Lista lista = new Lista();
         do {
-            System.out.println("Menu:");
+            System.out.println("Menu - Locadora Filme Bom:");
             System.out.println("1 - Adicionar dvd");
             System.out.println("2 - Verificar disponibilidade do filme");
             System.out.println("3 - Locação de DVD");
             System.out.println("4 - Devolução de DVD");
             System.out.println("5 - Venda de DVD");
-            System.out.println("6 - Sair");
+            System.out.println("6 - Mostra o catálogo");
+            System.out.println("7 - Sair");
 
             resp = in.nextInt();
             switch (resp) {
@@ -25,18 +27,28 @@ public class Main {
                     System.out.println("Digite o codigo do DVD");
                     codigo = in.nextInt();
                     System.out.println("Digite o titulo do filme: ");
-                    String titulo = in.next();                    
+                    String titulo = in2.nextLine();                    
                     System.out.println("Digite o ano do filme: ");
                     int ano = in.nextInt();                    
                     System.out.println("Digite o estilo do filme: ");
-                    String estilo = in.next();                    
+                    String estilo = in2.nextLine();                    
                     System.out.println();
-                    System.out.println("Filme adicionado com sucesso.");
-                    DVD dvd= new DVD(codigo, titulo, ano, estilo);                    
-                    Node aux = new Node(dvd);                                   
-                    lista.addEnd(aux);
+                    System.out.println("Cadastrar o filme: " +titulo+ " ? (0 - Sim / 1 - Não)");
+                    int resp2;
+                    resp2 = in.nextInt();
+                    if (resp2 == 0) {
+                        System.out.println("");
+                        System.out.println("Filme cadastrado com sucesso.");
+                        System.out.println("");
+                        DVD dvd= new DVD(codigo, titulo, ano, estilo);                    
+                        Node aux = new Node(dvd);                                   
+                        lista.addEnd(aux);
+                    } else {
+                        System.out.println("");
+                        System.out.println("Filme não cadastrado.");
+                        System.out.println("");
+                    }
                     
-                    lista.display();
                     break;
                     
                 case 2:
@@ -47,13 +59,13 @@ public class Main {
                     
                 case 3:
                     System.out.println("Digite o nome do filme que deseja alugar: ");
-                    nome = in.next();
+                    nome = in2.nextLine();
                     lista.alugar(nome);
 
                     break;
                 case 4:
                     System.out.println("Digite o nome do filme para devolução");
-                    nome = in.next();
+                    nome = in2.nextLine();
 
                     lista.devolver(nome);
 
@@ -65,27 +77,14 @@ public class Main {
                     break;
 
                 case 6:
-                    System.out.println("Fechando o programa:");
+                    lista.display();
                     break;
-
+                    
+                case 7:
+                    System.out.println("Fechando o programa.");
+                    break;
+                    
             }
-
-
-        } while ((resp >= 1)&& (resp < 6));
-        //Essa parte de baixo não é nescessaria... fiz o teste aqui e não deu errou ta td certo aqui em ksa sem essa parte.
-        /*
-        DVD dvd2 = new DVD(4, "aa", 0, "bb");
-        DVD dvd1 = new DVD(1, "bb", 0, "bb");
-        Node n = new Node(dvd2);
-        lista.addEnd(n);
-        Node a = new Node(dvd1);
-        lista.addEnd(a);
-        lista.disponibilidade(4);        
-        lista.disponibilidade(1);
-        lista.disponibilidade(0);
-        lista.disponibilidade(3);
-        lista.disponibilidade(4);
-        lista.disponibilidade(1);
-        lista.display();*/        
+        } while ((resp >= 1)&& (resp < 7));        
     }    
 }
